50168b9616b0134269249c1198ac023a587627f2
suuuuu017 <suuuuu017@ShiHes-MacBook-Pro.local>
2021-06-03 15:33:43 +0800

    finish skeleton

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -1,4 +1,38 @@
 //
 // Created by ShiHe Wang on 2021/6/3.
 //
+#include <iostream>
+#include <vector>
+#include <queue>
 
+class gate{
+protected:
+    bool val;
+    std::string name;
+    int inDegree;
+    std::vector<gate*> neighbour;
+    bool visited;
+
+public:
+    gate(std::string name){
+        name = name;
+    };
+
+    void addNeighbour(){
+
+    };
+
+    void logicOperation(){
+
+    };
+};
+
+std::queue<gate*> q;
+
+void readNetlist(){
+
+};
+
+int main(){
+
+};
