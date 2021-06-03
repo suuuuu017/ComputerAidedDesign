880e7ff9b521a77cbc3c331eb04fee6632b94109
suuuuu017 <suuuuu017@ShiHes-MacBook-Pro.local>
2021-06-03 21:00:19 +0800

    finish logic computation function

--- PA1/main.cpp
+++ PA1/main.cpp
@@ -4,26 +4,69 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <string>
 
 class gate{
 protected:
     std::string name;
     bool val;
     int inDegree;
-    std::vector<gate*> neighbour;
+    std::vector<gate*> parent;
+    std::vector<gate*> child;
     bool visited;
 
 public:
-    gate(std::string name){
+    gate(std::string name, bool val){
         name = name;
+        val = val;
     };
 
     void addNeighbour(){
 
     };
 
-    void logicOperation(){
-
+    void logicOperation(const char* &op, std::vector<gate*> input, bool output){
+        output = (*input.begin())->val;
+        if(strcmp(op, "AND") == 0 || strcmp(op, "and") == 0){
+            for(auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output & (*it)->val;
+            }
+        }
+        if(strcmp(op, "OR") == 0 || strcmp(op, "or") == 0){
+            for(auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output | (*it)->val;
+            }
+        }
+        else if(strcmp(op, "NAND") == 0 || strcmp(op, "nand") == 0){
+            for(auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output & (*it)->val;
+            }
+            output = !output;
+        }
+        else if(strcmp(op, "NOR") == 0 || strcmp(op, "nor") == 0){
+            for(auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output | (*it)->val;
+            }
+            output = !output;
+        }
+        else if(strcmp(op, "XOR") == 0 || strcmp(op, "xor") == 0) {
+            for (auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output ^ (*it)->val;
+            }
+        }
+        else if(strcmp(op, "XNOR") == 0 || strcmp(op, "xnor") == 0) {
+            for (auto it = ++input.begin(); it != input.end(); ++it) {
+                output = output ^ (*it)->val;
+            }
+            output = !output;
+        }
+        else if(strcmp(op, "NOT") == 0 || strcmp(op, "not") == 0) {
+            output = !output;
+        }
+        else if (strcmp(op, "BUF") == 0 || strcmp(op, "buf") == 0) {
+            output = output;
+        }
+        this->val = output;
     };
 };
 
